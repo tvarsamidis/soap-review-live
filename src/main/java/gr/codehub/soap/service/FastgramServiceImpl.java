b6066916db769754f55e90c99e57e5a1cbfc6dfb
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-09-18 10:58:03 +0300

    Preparation for REST - beans.xml web.xml Changed implementation of FastgramServiceImpl

--- src/main/java/gr/codehub/soap/service/FastgramServiceImpl.java
+++ src/main/java/gr/codehub/soap/service/FastgramServiceImpl.java
@@ -2,12 +2,15 @@ package gr.codehub.soap.service;
 
 import gr.codehub.soap.model.FastgramPost;
 import gr.codehub.soap.repository.FastgramRepository;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 
 import java.util.List;
 
-@WebService(endpointInterface = "gr.codehub.soap.service.FastgramService")
+//@ApplicationScoped
+//@SessionScoped
+@RequestScoped
 public class FastgramServiceImpl implements FastgramService {
 
     private static final FastgramRepository fastgramRepository;
@@ -16,43 +19,36 @@ public class FastgramServiceImpl implements FastgramService {
         fastgramRepository = new FastgramRepository();
     }
 
-    @WebMethod
     @Override
     public FastgramPost findPostById(int id) {
         return fastgramRepository.findPostById(id);
     }
 
-    @WebMethod
     @Override
     public FastgramPost findPostByMaxViews() {
         return fastgramRepository.findPostByMaxViews();
     }
 
-    @WebMethod
     @Override
     public List<FastgramPost> findPostsByUser(String user) {
         return fastgramRepository.findPostsByUser(user);
     }
 
-    @WebMethod
     @Override
     public List<FastgramPost> findPostsByContent(String content) {
         return fastgramRepository.findPostsByContent(content);
     }
 
-    @WebMethod
     @Override
     public FastgramPost savePost(FastgramPost currency) {
         return fastgramRepository.savePost(currency);
     }
 
-    @WebMethod
     @Override
     public FastgramPost deletePostById(int id) {
         return fastgramRepository.deletePostById(id);
     }
 
-    @WebMethod
     @Override
     public FastgramPost undeletePostById(int id) {
         return fastgramRepository.undeletePostById(id);
