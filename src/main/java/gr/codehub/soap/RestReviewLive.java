f9be34533d26e5807ad46b33857ccdfa651b02da
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-09-18 10:43:20 +0300

    Preparation for REST - pom.xml

---
+++ src/main/java/gr/codehub/soap/RestReviewLive.java
@@ -0,0 +1,9 @@
+package gr.codehub.soap;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("appPath")
+public class RestReviewLive extends Application {
+
+}
